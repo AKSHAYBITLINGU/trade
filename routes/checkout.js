74eb4dd35dbbff9773eb4dd776b200a3330113f2
AKSHAYBITLINGU <akshaybitlingu786@gmail.com>
2024-06-04 02:44:52 +0530

    add checkout logic and modified style.css

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../models/user")
 const PaymentService = require('../services/payment'); // Implement payment service
 
 // Route to display cart and collect address
 router.get('/', async (req, res) => {
   try {
-    const cart = await Cart.getCart(req.user.id);
-    res.render('checkout', { cart, user: req.user });
+    if (!res.locals.isLoggedIn) {
+      return res.redirect("/login"); // Redirect if user is not logged in
+    }
+    if (!req.user || !req.user.userId) {
+      console.error("Error: req.user or req.user._id is undefined");
+      return res.redirect("/login");
+    }
+
+    const userId = req.user.userId; 
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).send("User not found"); 
+    }
+    const cartItems = user.cart; 
+    let totalQuantity = 0;
+    let totalPrice = 0;
+
+    cartItems.forEach(item => {
+        totalQuantity += item.quantity;
+        totalPrice += item.quantity * item.price;
+    });
+
+    res.render('checkout', {
+        cartItems: cartItems,
+        user: user,
+        totalQuantity: totalQuantity,
+        totalPrice: totalPrice
+    });
   } catch (error) {
     console.error('Error displaying cart:', error);
     res.status(500).send('Internal server error');
